006161f2dbb35fbcba754a9c2302af0bef89c427
Isaac Lewis <mail@isaaclewis.me>
2023-02-04 20:57:19 -0600

    postfix added

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,95 @@
-use std::io;
+use std::{io, vec};
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub enum Associativity {
     Right,
     Left,
     None,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, PartialEq)]
+enum TokenType {
+    Float,     // all numbers are treated as floats for simplicity
+    Function,  // Only pre-defined functions are allowed
+    Separator, // Things like parenthesis or logic
+    Operator,  // Things like + - * /
+}
+
+#[derive(Debug, Clone)]
 struct Token {
-    value: String,
+    keyword: String,
+    token_type: TokenType,
+    parameters: usize,
     associativity: Associativity,
     precedence: usize,
 }
 
 #[derive(Clone)]
 struct KnownToken<'a> {
-    value: &'a str,
+    keyword: &'a str,
+    token_type: TokenType,
+    parameters: usize,
     associativity: Associativity,
     precedence: usize,
 }
 
 const ACCEPTABLE_SPECIAL: [char; 1] = ['.'];
 
-const KNOWN_TOKENS: [KnownToken; 7] = [
+const KNOWN_TOKENS: [KnownToken; 8] = [
     KnownToken {
-        value: "+",
+        keyword: "+",
+        token_type: TokenType::Operator,
         associativity: Associativity::Left,
+        parameters: 2,
         precedence: 2,
     },
     KnownToken {
-        value: "-",
+        keyword: "-",
+        token_type: TokenType::Operator,
         associativity: Associativity::Left,
+        parameters: 2,
         precedence: 2,
     },
     KnownToken {
-        value: "*",
+        keyword: "*",
+        token_type: TokenType::Operator,
         associativity: Associativity::Left,
+        parameters: 2,
         precedence: 3,
     },
     KnownToken {
-        value: "/",
+        keyword: "/",
+        token_type: TokenType::Operator,
         associativity: Associativity::Left,
+        parameters: 2,
         precedence: 3,
     },
     KnownToken {
-        value: "^",
+        keyword: "^",
+        token_type: TokenType::Operator,
         associativity: Associativity::Right,
+        parameters: 2,
         precedence: 4,
     },
     KnownToken {
-        value: "(",
+        keyword: "(",
+        token_type: TokenType::Separator,
         associativity: Associativity::None,
+        parameters: 0,
         precedence: 1,
     },
     KnownToken {
-        value: ")",
+        keyword: ")",
+        token_type: TokenType::Separator,
         associativity: Associativity::None,
+        parameters: 0,
+        precedence: 1,
+    },
+    KnownToken {
+        keyword: "SUM",
+        token_type: TokenType::Function,
+        associativity: Associativity::Left,
+        parameters: 1,
         precedence: 1,
     },
 ];
@@ -71,72 +104,29 @@ fn main() {
     }
 
     // Create tokens
-    let tokens = create_tokens(&input);
-
-    println!("{:?}", tokens);
+    let mut tokens = create_tokens(&input);
+    print_pretty_tokens(&tokens);
     // Create postfix
+    let mut postfix_tokens = create_postfix(&mut tokens);
+    print_pretty_tokens(&postfix_tokens);
 
     // Solve postfix
+    // let answer: f64 = solve_postfix(&mut postfix_tokens);
+    // println!("Answer: {}", answer)
 }
 
-fn peek(vec: &Vec<char>) -> &char {
-    match vec.last() {
-        Some(x) => &x,
-        None => {
-            panic!("Failed peek!")
-        }
+fn print_pretty_tokens(tokens: &Vec<Token>) {
+    let mut pretty_output: String = String::from("");
+    let mut temp: Vec<String> = vec![];
+    for x in tokens {
+        temp.push(x.keyword.clone().to_string());
     }
-}
-
-fn save_and_clear_cache(cache: &mut Vec<char>, save: &mut Vec<Token>) {
-    let str_cache = String::from_iter(cache.iter());
-    println!("save_and_clear_cache: {:?}", str_cache);
-    cache.clear();
-
-    match KNOWN_TOKENS.iter().find(|e| e.value == str_cache) {
-        Some(x) => {
-            let x = x.clone();
-            let y: Token = Token {
-                value: String::from(x.value),
-                associativity: x.associativity,
-                precedence: x.precedence,
-            };
-            save.push(y)
-        }
-        None => save.push(Token {
-            value: str_cache,
-            associativity: Associativity::Left,
-            precedence: 1,
-        }),
-    };
-}
-
-fn is_alphabetic_char(c: &char) -> bool {
-    matches!(c, 'a'..='z' | 'A'..='Z')
-}
-fn is_numeric_char(c: &char) -> bool {
-    matches!(c, '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9')
-}
-
-fn is_known_token(c: &char) -> bool {
-    for o in KNOWN_TOKENS {
-        if o.value == c.to_string() {
-            return true;
-        }
-    }
-    false
-}
-
-fn is_acceptable_special_char(c: &char) -> bool {
-    for o in ACCEPTABLE_SPECIAL {
-        if &o == c {
-            return true;
-        }
-    }
-    false
+    pretty_output = temp.join(" ");
+    println!("{:?}", &pretty_output);
 }
 
 fn create_tokens(input: &String) -> Vec<Token> {
+    println!("\nCreating Tokens...");
     let mut answer: Vec<Token> = vec![];
     let mut cache: Vec<char> = vec![];
     let input = input
@@ -150,8 +140,8 @@ fn create_tokens(input: &String) -> Vec<Token> {
     // println!("{:?}", input_chars);
 
     // Make sure the open and close parenthesis count are the same
-    let open_p_count: usize = input_chars.iter().filter(|&y| y.clone() == '(').count();
-    let close_p_count: usize = input_chars.iter().filter(|&y| y.clone() == ')').count();
+    let open_p_count: usize = input_chars.iter().filter(|&y| y == &'(').count();
+    let close_p_count: usize = input_chars.iter().filter(|&y| y == &')').count();
     assert_eq!(open_p_count, close_p_count);
 
     // Check for valid characters before starting.
@@ -165,8 +155,87 @@ fn create_tokens(input: &String) -> Vec<Token> {
         }
     }
 
+    // Returns the top character from a vector
+    fn peek(vec: &Vec<char>) -> &char {
+        match vec.last() {
+            Some(x) => &x,
+            None => {
+                panic!("Failed peek!")
+            }
+        }
+    }
+
+    // Converts the cache vector into a token to save and clears the cache
+    fn save_and_clear_cache(cache: &mut Vec<char>, save: &mut Vec<Token>) {
+        let str_cache = String::from_iter(cache.iter());
+        // println!("save_and_clear_cache: {:?}", str_cache);
+        cache.clear();
+
+        match KNOWN_TOKENS.iter().find(|e| e.keyword == str_cache) {
+            // Know tokens are saved into the answer
+            Some(x) => {
+                // println!("Provided token is know: {}", str_cache);
+                let x = x.clone();
+                let y: Token = Token {
+                    keyword: String::from(x.keyword),
+                    token_type: x.token_type,
+                    parameters: x.parameters, // Change to be function accurate?
+                    associativity: x.associativity,
+                    precedence: x.precedence,
+                };
+                save.push(y)
+            }
+            // Unknown tokens are saved if they can be parsed into a float
+            None => {
+                if str_cache.parse::<f64>().is_ok() {
+                    // println!("Provided token assigned TokenType::Float: {}", str_cache);
+                    save.push(Token {
+                        keyword: str_cache,
+                        token_type: TokenType::Float,
+                        parameters: 0,
+                        associativity: Associativity::None,
+                        precedence: 1,
+                    });
+                } else {
+                    panic!("Provided token was unidentifiable!")
+                }
+            }
+        };
+    }
+
+    // Ensures a character is precisely a-z or A-Z
+    fn is_alphabetic_char(c: &char) -> bool {
+        matches!(c, 'a'..='z' | 'A'..='Z')
+    }
+
+    // Ensures a character is precisely 0-9
+    fn is_numeric_char(c: &char) -> bool {
+        matches!(c, '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9')
+    }
+
+    // Ensures a character matches a pre-defined, know token's character
+    fn is_known_token(c: &char) -> bool {
+        for o in KNOWN_TOKENS {
+            if o.keyword == c.to_string() {
+                return true;
+            }
+        }
+        false
+    }
+
+    // Ensures a character is an acceptable special character, such as decimal
+    fn is_acceptable_special_char(c: &char) -> bool {
+        for o in ACCEPTABLE_SPECIAL {
+            if &o == c {
+                return true;
+            }
+        }
+        false
+    }
+
+    // Loop through the characters and save tokens for them.
     while input_chars.len() > 0 {
-        println!("");
+        // println!("");
         let x: char = match input_chars.pop() {
             Some(x) => x,
             None => {
@@ -220,5 +289,101 @@ fn create_tokens(input: &String) -> Vec<Token> {
     }
     answer
 }
-// fn create_postfix(tokens: Vec<token>) -> Vec<token> {}
-// fn solve_postfix(tokens: Vec<token>) -> f64 {}
+
+fn create_postfix(tokens: &mut Vec<Token>) -> Vec<Token> {
+    println!("\nCreating Postfix...");
+    let mut operator_stack: Vec<Token> = vec![];
+    let mut output_queue: Vec<Token> = vec![];
+
+    fn pop_and_push(from: &mut Vec<Token>, to: &mut Vec<Token>) {
+        let a = match from.pop() {
+            Some(b) => b,
+            None => panic!("Unable to pop 'from'"),
+        };
+        to.push(a);
+    }
+
+    // Returns a copy of the top token from a vector
+    // Makes a copy because I had clashing mut ref to output
+    fn peek(vec: &Vec<Token>) -> Token {
+        match vec.last() {
+            Some(x) => x.clone(),
+            None => {
+                panic!("Failed peek!")
+            }
+        }
+    }
+
+    // reverse the input tokens so we can pop tokens off as we cycle through them
+    tokens.reverse();
+
+    while tokens.len() > 0 {
+        // println!("\n\t\toperator_stack: {:?}", operator_stack);
+
+        let x: Token = match tokens.pop() {
+            Some(x) => x,
+            None => {
+                println!("No token found!");
+                continue;
+            }
+        };
+        // println!("looking at token: {:?}", x);
+
+        match x.token_type {
+            TokenType::Float => {
+                // println!("Pushed to output");
+                output_queue.push(x)
+            }
+            TokenType::Function => {
+                // println!("Pushed to output");
+                output_queue.push(x)
+            }
+            TokenType::Operator => {
+                while (operator_stack.len() > 0)
+                    && peek(&operator_stack).keyword != ")"
+                    && ((peek(&operator_stack).precedence >= x.precedence)
+                        || ((peek(&operator_stack).precedence == x.precedence)
+                            && (x.associativity == Associativity::Left)))
+                {
+                    // println!("Popping and pushing to output -  Operator");
+                    pop_and_push(&mut operator_stack, &mut output_queue)
+                }
+                // println!("Pushing to operator stack -  Operator");
+                operator_stack.push(x);
+            }
+            TokenType::Separator => {
+                // println!("X: {:?}", x);
+                if x.keyword == "(" {
+                    // println!("Pushed to stack - Separator");
+                    operator_stack.push(x);
+                } else if x.keyword == ")" {
+                    while (operator_stack.len() > 0) && (peek(&operator_stack).keyword != "(") {
+                        assert!(&operator_stack.len() > &0);
+                        // println!("Popping and pushing to output -  Separator");
+                        pop_and_push(&mut operator_stack, &mut output_queue)
+                    }
+
+                    assert!((operator_stack.len() > 0) && (peek(&operator_stack).keyword == "("));
+                    // println!("Popping Separator (");
+                    operator_stack.pop();
+
+                    if (operator_stack.len() > 0)
+                        && peek(&operator_stack).token_type == TokenType::Function
+                    {
+                        // println!("Popping and pushing to output - Function");
+                        pop_and_push(&mut operator_stack, &mut output_queue)
+                    }
+                }
+            }
+        }
+    }
+    while operator_stack.len() > 0 {
+        // println!("remaining operator onto the output queue");
+        assert!((operator_stack.len() > 0) && peek(&operator_stack).keyword != "(");
+        pop_and_push(&mut operator_stack, &mut output_queue)
+    }
+    output_queue
+}
+
+// fn solve_postfix(tokens: &mut Vec<Token>) -> f64 {
+// }
